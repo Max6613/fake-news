15b0ed0c7d8c8b331bd784235788f19e7b93f227
Max6613 <maxime66110@gmail.com>
2020-06-24 12:17:30 +0200

    header: manque script aff/masquage menu, traits séparations, style burger btn

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+let burgerCheck = document.querySelector("#burger-cmd");
+burgerCheck.addEventListener("")
+
+$("input[name=burger-check]").change(function () {
+    if ($(this).is(":checked")){
+        // #main-menu display block
+    } else {
+        // #main-menu display none
+    }
+});
