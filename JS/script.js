51589c974674c2c6d0311cf6701ca6587f5ff6be
Max6613 <maxime66110@gmail.com>
2020-07-01 11:02:39 +0200

    JS pour gérer l'agrandissement de la fenetre avec menu ouvert, animation menu

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,25 @@
-// affiche ou masque le menu nav en fonction de l'etat de la checkbox'
-$("input[name=burger-check]").change(function () {
-    if ($(this).is(":checked")){
-        $("body").addClass("open");
-    } else {
-        $("body").removeClass("open");
-    }
+$(document).ready(function () {
+    // affiche ou masque le menu nav en fonction de l'etat de la checkbox'
+    $("input[name=burger-check]").change(function () {
+        if ($(this).is(":checked")){
+            $("body").addClass("open");
+        } else {
+            $("body").removeClass("open");
+        }
+    });
+
+
+    /* Si agrandissement de la fenetre en mode bureau avec le menu déployer,
+   déplacement à la position d'origine */
+    $(window).resize(function () {
+        if($(window).width() > 991 && $("#burger-cmd").prop("checked")){
+            $("#burger-cmd").prop("checked", false);
+
+            /* Le changement d'état de la checkbox via JS
+               ne déclenche pas l'event change */
+            $("body").removeClass("open");
+        }
+    });
 });
 
 
